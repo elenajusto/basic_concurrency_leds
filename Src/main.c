71d0b80a6b6db0ddb4b74123479135dd7a1e8f1d
Elena Justo <elena11j@protonmail.ch>
2026-06-07 10:15:17 +1000

    remove old gpio led and button code

--- Src/main.c
+++ Src/main.c
@@ -4,24 +4,10 @@
 bool btn_state;
 
 int main(void) {
-	/*Initialize LED*/
-	led_init();
-
-	/*Initialize Pushbutton*/
-	button_init();
-
 	/* Init port b */
 	init_port_b();
 
 	while(1) {
-		/*Get Pushbutton State*/
-		btn_state = get_btn_state();
-		if(btn_state) {
-			led_on();
-		} else {
-			led_off();
-		}
-
 		pin_high(GPIOB, 4);
 		delay_ms(500);
 		pin_low(GPIOB, 4);
