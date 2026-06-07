54d35248b6cebdfc33782b4cff40ff904f791d58
Elena Justo <elena11j@protonmail.ch>
2026-06-07 10:07:11 +1000

    test blink using port b and generalised gpio init and set high/low code

--- Src/main.c
+++ Src/main.c
@@ -1,4 +1,5 @@
 #include "gpio.h"
+#include "delay.h"
 
 bool btn_state;
 
@@ -9,6 +10,9 @@ int main(void) {
 	/*Initialize Pushbutton*/
 	button_init();
 
+	/* Init port b */
+	init_port_b();
+
 	while(1) {
 		/*Get Pushbutton State*/
 		btn_state = get_btn_state();
@@ -17,5 +21,14 @@ int main(void) {
 		} else {
 			led_off();
 		}
+
+		pin_high(GPIOB, 4);
+		delay_ms(500);
+		pin_low(GPIOB, 4);
+		delay_ms(500);
+		pin_high(GPIOB, 3);
+		delay_ms(500);
+		pin_low(GPIOB, 3);
+		delay_ms(500);
 	}
 }
