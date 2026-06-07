eaa6e92bc53e507a431cd006772639daebcdefd1
Elena Justo <elena11j@protonmail.ch>
2026-06-07 12:01:41 +1000

    clean comments in main.c, make program cycle through lights

--- Src/main.c
+++ Src/main.c
@@ -6,26 +6,30 @@
 void init_port_b(void);
 void init_port_a(void);
 
-/* Main Program */
+/* Main Program Entry */
 int main(void) {
-	/* Init gpip ports */
+	/* Init gpio ports */
 	init_port_b();
 	init_port_a();
-
-	pin_high(GPIOB, 4);
-	pin_low(GPIOB, 4);
-
-	pin_high(GPIOB, 3);
-	pin_low(GPIOB, 3);
-
-	pin_high(GPIOA, 10);
-	pin_low(GPIOA, 10);
-
+	
+	/* Main Program Loop */
 	while(1) {
+		pin_high(GPIOB, 4);
+		delay_ms(200);
+		pin_low(GPIOB, 4);
+
+		pin_high(GPIOB, 3);
+		delay_ms(200);
+		pin_low(GPIOB, 3);
+
+		pin_high(GPIOA, 10);
+		delay_ms(200);
+		pin_low(GPIOA, 10);
 
 	}
 }
 
+/* Function definitions */
 void init_port_b(void) {
 	RCC->IOPENR |= RCC_IOPENR_GPIOBEN ;
 	init_pin(GPIOB, 4, GPIO_MODE_OUTPUT);  // D5
