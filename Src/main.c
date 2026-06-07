921e5fb711a45929d6c0edd014f39944d21dc425
Elena Justo <elena11j@protonmail.ch>
2026-06-07 14:52:22 +1000

    implement starter program as per book

--- Src/main.c
+++ Src/main.c
@@ -9,10 +9,15 @@
 #define BUTTON_ONE_PIN	9
 #define BUTTON_TWO_PIN	7
 
+#define W_DELAY_SLOW 	400
+#define W_DELAY_FAST 	200
+#define RGB_DELAY_SLOW 	4000
+#define RGB_DELAY_FAST	1000
+
 /* Prototypes */
 void init_port_b(void);
 void init_port_a(void);
-void control_rgb_Led(int red, int green, int blue);
+void control_rgb_led(int red, int green, int blue);
 
 /* Main Program Entry */
 int main(void) {
@@ -20,21 +25,36 @@ int main(void) {
 	init_port_b();
 	init_port_a();
 	
+	/* Init delay values */
+	uint32_t w_delay = W_DELAY_SLOW;
+	uint32_t rgb_delay = RGB_DELAY_SLOW;
+
 	/* Main Program Loop */
 	while(1) {
-		if (pin_read(GPIOA, BUTTON_ONE_PIN)) {
-			control_rgb_Led(1, 0, 0);
+
+		if (pin_read(GPIOA, BUTTON_ONE_PIN)) { // flash white
+			control_rgb_led(1, 1, 1);
+			delay(w_delay);
+			control_rgb_led(0, 0, 0);
+			delay(w_delay);
 		}
-		else {
-			control_rgb_Led(0, 0, 0);
+		else {									// sequence RGB
+			control_rgb_led(1, 0, 0);
+			delay(rgb_delay);
+			control_rgb_led(0, 1, 0);
+			delay(rgb_delay);
+			control_rgb_led(0, 0, 1);
+			delay(rgb_delay);
 		}
 
 		if (pin_read(GPIOA, BUTTON_TWO_PIN)) {
-			control_rgb_Led(0, 1, 0);
+			w_delay = W_DELAY_FAST;
+			rgb_delay = RGB_DELAY_FAST;
+		} else {
+			w_delay = W_DELAY_SLOW;
+			rgb_delay = RGB_DELAY_SLOW;
 		}
-		else {
-			control_rgb_Led(0, 0, 0);
-		}
+
 	}
 }
 
@@ -52,7 +72,7 @@ void init_port_a(void) {
 	init_pin(GPIOA, BUTTON_TWO_PIN, GPIO_MODE_INPUT);   // D11
 }
 
-void control_rgb_Led(int red_on, int green_on, int blue_on) {
+void control_rgb_led(int red_on, int green_on, int blue_on) {
 	if (red_on) {
 		pin_high(GPIOB, RED_LED_PIN);
 	} else {
