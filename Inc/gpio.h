dfa3f78fcda7c91fe2ae426d200cccbb208fc8ce
Elena Justo <elena11j@protonmail.ch>
2026-06-07 10:05:46 +1000

    gpio typedef enum for setting mode, generaliseed function for init pin and port, generalised function for setting pin high and low

--- Inc/gpio.h
+++ Inc/gpio.h
@@ -11,4 +11,17 @@ void led_off(void);
 void button_init(void);
 bool get_btn_state(void);
 
+typedef enum {
+    GPIO_MODE_INPUT  = 0,
+    GPIO_MODE_OUTPUT = 1,
+    GPIO_MODE_AF     = 2,
+    GPIO_MODE_ANALOG = 3
+} gpio_mode_t;
+
+void init_pin(GPIO_TypeDef *port, uint8_t pin, gpio_mode_t mode);
+void init_port_b(void);
+
+void pin_high(GPIO_TypeDef *port, uint8_t pin);
+void pin_low(GPIO_TypeDef *port, uint8_t pin);
+
 #endif /* GPIO_H_ */
